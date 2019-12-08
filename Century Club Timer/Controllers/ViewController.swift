64190150c94bb45177ccb65194f8c973649e2efb
zjradfor <zjradfor@uwaterloo.ca>
2019-12-08 15:27:00 -0500

    alert actions

--- Century Club Timer/Controllers/ViewController.swift
+++ Century Club Timer/Controllers/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var progressBar: ProgressBar!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
     
     @IBOutlet weak var tableview: UITableView!
     
@@ -41,13 +42,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func timerDone() {
         count += 1
+        counterLabel.text = "\(count)"
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
         
-        if count == 2 {
+        // this should go 1 more than 100 to account for drink 100 then show a finish screen
+        if count == 3 {
             countdownTimerDidStart = false
             startButton.setTitle("START",for: .normal)
             count = 0
-            
-            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+            counterLabel.text = "0"
             
             print("countdownTimerDone")
         } else {
@@ -65,6 +68,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             countdownTimerDidStart = false
             startButton.setTitle("RESUME",for: .normal)
         }
+    }
+    @IBAction func addPlayerPressed() {
+        let alert = UIAlertController(title: "Add Player", message: nil, preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { alertAction in }))
+
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { [weak alert] (_) in
+            if let textField = alert?.textFields?[0] {
+                if textField.text != "" {
+                    print("\(textField.text ?? "default")")
+                }
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func endGamePressed() {
+        let alert = UIAlertController(title: "End Game", message: "The current game will end, are you sure?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "No", style: .default, handler: { alertAction in }))
+
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { [weak alert] (_) in
+            // do stuff to end game
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     
     func startTimer() {
